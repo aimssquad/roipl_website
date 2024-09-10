c2e51c50fbff315fe06a33b55ca3c25ad91a23df
aimssquad <abbasbitm3655@gmail.com>
2024-09-10 10:05:53 +0530

    updated admin-ui

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\RoleController;
 
+Route::middleware(['admin_guest'])->prefix('/admin/')->name('admin.')->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('login', 'index')->name('login');
+        Route::post('login', 'login')->name('login.post');
+    });
+
+});
 
 Route::middleware(['admin_auth:admin'])->prefix('/admin/')->name('admin.')->group(function () {
     Route::controller(AdminController::class)->group(function () {
@@ -11,11 +19,7 @@ Route::middleware(['admin_auth:admin'])->prefix('/admin/')->name('admin.')->grou
         Route::get('dashboard', 'dashboard')->name('dashboard');
         Route::get('profile', 'profile')->name('profile');
     });
+    Route::resource('roles', RoleController::class)->except(['show']);
 });
 
-Route::middleware(['admin_guest'])->prefix('/admin/')->name('admin.')->group(function () {
-    Route::controller(LoginController::class)->group(function () {
-        Route::get('login', 'index')->name('login');
-        Route::post('login', 'login')->name('login.post');
-    });
-});
+
