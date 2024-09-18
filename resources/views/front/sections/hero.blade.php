e02107fb22fbfddb4c571544d716e1a4b002af2f
aimssquad <abbasbitm3655@gmail.com>
2024-09-18 14:37:59 +0530

    updated

--- resources/views/front/sections/hero.blade.php
+++ resources/views/front/sections/hero.blade.php
@@ -1,5 +1,5 @@
 <section id="hero" class="hero section dark-background">
-    <img src="https://images.unsplash.com/photo-1509695507497-903c140c43b0?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" data-aos="fade-in">
+    <img src="{{ asset('assets/img/banner.JPG') }}" alt="" data-aos="fade-in">
     <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
         <div class="row justify-content-center">
             <div class="col-lg-8">
