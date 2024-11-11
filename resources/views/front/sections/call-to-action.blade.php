6b11b6e347a280c65d58d51f86fa73b7e1b5f825
aimssquad <abbasbitm3655@gmail.com>
2024-11-11 11:42:37 +0530

    updated call to action image

--- resources/views/front/sections/call-to-action.blade.php
+++ resources/views/front/sections/call-to-action.blade.php
@@ -1,6 +1,6 @@
 <section id="call-to-action" class="call-to-action section dark-background">
 
-    <img src="{{asset('assets/img/Lobby1.jpg')}}" alt="">
+    <img src="{{asset('assets/img/callto.webp')}}" alt="">
 
     <div class="container">
       <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
