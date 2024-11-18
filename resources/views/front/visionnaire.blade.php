e7670122341217a26750d6d2fef491867f0a8cc9
aimssquad <abbasbitm3655@gmail.com>
2024-11-18 12:20:27 +0530

    updated

--- resources/views/front/visionnaire.blade.php
+++ resources/views/front/visionnaire.blade.php
@@ -10,6 +10,6 @@
 @section('content')
     @include('front.partials.page-title', ['title' => 'Visionnaire', 'current' => 'Visionnaire'])
     @include('front.sections.vission')
-    @include('front.sections.vission-gallery')
+    {{-- @include('front.sections.vission-gallery') --}}
 
 @endsection
