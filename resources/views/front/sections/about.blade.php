78938e24b5249a1e4bb0fcc8535255283101c712
aimssquad <abbasbitm3655@gmail.com>
2024-11-18 10:31:56 +0530

    updated

--- resources/views/front/sections/about.blade.php
+++ resources/views/front/sections/about.blade.php
@@ -2,7 +2,14 @@
     <div class="container section-title" data-aos="fade-up" style="margin-top: 0px !important;padding: 30px 0 !important;margin-bottom: 30px !important; ">
         <span class="description-title">About Us</span>
         <h2>About Us</h2>
-        <p style="text-align: justify;font-weight: 500;">Ronak Optik India Private Limited is a  eyewear business with over a quarter-century of experience in the industry. Established in 1993 with a commitment to quality and customer satisfaction, we’ve grown to become one of India’s largest eyewear  distributors, and offer a vast collection of in-house brands such as IDEE, IRUS and IMAGE eyewear, as well as international brands.
-        </p><p style="text-align: justify;font-weight: 500;"> Ronak Optik, we believe in serving everyone, from the   more discerning buyer to the luxury customer.  With Ronak Optik, you’ll find a varied stock of frames ranging from quality in-house to international brands. Our extensive collection is curated to meet diverse tastes and preferences, , guaranteeing that every customer finds their perfect fit.  . Ronak Optik is committed to providing eyewear that combines quality, comfort and style, all within flexible price ranges.</p>
+        <p style="text-align: justify;font-weight: 500;">Ronak Optik India Private Limited is an eyewear business with over a
+            quarter-century of experience in the industry. Established in 1993 with a commitment to quality and customer
+            satisfaction, we’ve grown to become one of India’s largest eyewear  distributors, and offer a vast collection
+            of in-house brands such as IDEE, IRUS and IMAGE eyewear, as well as several international brands.
+        <br></p><p style="text-align: justify;font-weight: 500;"> Here at Ronak Optik, we believe in serving everyone, from the
+            more discerning buyer to the luxury customer.  At Ronak Optik, you’ll find a
+            varied range of frames - from quality in-house to international brands. Our extensive collection
+             is curated to meet diverse tastes and preferences, , guaranteeing that every customer finds their perfect
+              fit. Ronak Optik is committed to providing eyewear that combines quality, comfort and style.</p>
     </div>
 </section>
