26f1be78be95ad7f81b710f88c0192a31b580516
aimssquad <abbasbitm3655@gmail.com>
2024-12-26 11:57:05 +0530

    updated

--- resources/views/front/sections/featured.blade.php
+++ resources/views/front/sections/featured.blade.php
@@ -2,31 +2,42 @@
 
     <div class="container">
 
-        @foreach($datas as $index => $data)
+        <!-- Top Margin -->
+        <div class="row mb-4"></div>
+
         <div class="row gy-4 align-items-center features-item">
-            @if($index % 2 == 0)
-                <!-- Text first, then Image -->
-
-                <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                    <h3>{{ $data->title }}</h3>
-                    <p class="fst-italic">{!! $data->description !!}</p>
-                </div>
-                <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                    <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
-                </div>
-            @else
-                <!-- Image first, then Text -->
-
-                <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
-                    <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
-                </div>
-                <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
-                    <h3>{{ $data->title }}</h3>
-                    <p class="fst-italic">{!! $data->description !!}</p>
-                </div>
-            @endif
+            @foreach($datas as $index => $data)
+                @if($index % 2 == 0)
+                    <!-- Text first, then Image -->
+                    <div class="col-md-1"></div> <!-- Left margin -->
+                    <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+                        <h3>{{ $data->title }}</h3>
+                        <p class="fst-italic">{!! $data->description !!}</p>
+                    </div>
+                    <div class="col-md-3 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-1"></div> <!-- Right margin -->
+                @else
+                    <!-- Image first, then Text -->
+                    <div class="col-md-1"></div> <!-- Left margin -->
+                    <div class="col-md-3 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                        <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+                        <h3>{{ $data->title }}</h3>
+                        <p class="fst-italic">{!! $data->description !!}</p>
+                    </div>
+                    <div class="col-md-1"></div> <!-- Right margin -->
+                @endif
+            @endforeach
         </div>
-    @endforeach
+
+        <!-- Bottom Margin -->
+        <div class="row mt-4"></div>
+
+    </div>
+
 
 
     </div>
