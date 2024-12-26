be79205e8c91f892d5b2e24d787f68fefddfd191
aimssquad <abbasbitm3655@gmail.com>
2024-12-26 11:45:12 +0530

    updated

--- resources/views/front/sections/featured.blade.php
+++ resources/views/front/sections/featured.blade.php
@@ -6,19 +6,21 @@
         <div class="row gy-4 align-items-center features-item">
             @if($index % 2 == 0)
                 <!-- Text first, then Image -->
-                <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+
+                <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
                     <h3>{{ $data->title }}</h3>
                     <p class="fst-italic">{!! $data->description !!}</p>
                 </div>
-                <div class="col-md-5 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
                     <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
                 </div>
             @else
                 <!-- Image first, then Text -->
-                <div class="col-md-5 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+
+                <div class="col-md-4 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
                     <img src="{{ asset('storage/'.$data->image) }}" class="img-fluid" alt="">
                 </div>
-                <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-md-8" data-aos="fade-up" data-aos-delay="100">
                     <h3>{{ $data->title }}</h3>
                     <p class="fst-italic">{!! $data->description !!}</p>
                 </div>
