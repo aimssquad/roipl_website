d7a5f228a0d1a83137ac8244720ade3ea0d803a1
aimssquad <abbasbitm3655@gmail.com>
2024-12-27 12:24:03 +0530

    updated

--- resources/views/front/sections/hero.blade.php
+++ resources/views/front/sections/hero.blade.php
@@ -1,6 +1,6 @@
 <section id="hero" class="hero section dark-background">
-    <img src="{{ asset('assets/img/Lobby3.jpg') }}" alt="" class="desktop-img" data-aos="fade-in">
-    <img src="{{ asset('assets/img/Lobby4.jpg') }}" alt="" class="mobile-img" data-aos="fade-in">
+    <img src="{{ asset('assets/img/Lobby3.webp') }}" alt="" class="desktop-img" data-aos="fade-in">
+    <img src="{{ asset('assets/img/Lobby4.webp') }}" alt="" class="mobile-img" data-aos="fade-in">
     <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
         <div class="row justify-content-center">
             <div class="col-lg-8">
