5f2a721df71ddbd2d7e3f3e54bf21644b4a7c7e6
aimssquad <abbasbitm3655@gmail.com>
2024-12-27 11:51:20 +0530

    updated

--- resources/views/front/visionnaire-detail.blade.php
+++ resources/views/front/visionnaire-detail.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 @include('front.partials.page-title', ['title' => $data->title, 'current' => $data->title])
+
+<div class="page-title dark-background">
+    <div class="container">
+        <h1>{{ $data->title }}</h1>
+        <h3>{{ $data->title }} </h3>
+    </div>
+</div>
     @include('front.sections.visionnaire-detail')
 
 
