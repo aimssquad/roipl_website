97c68ee459807a9ae075998d250601927f025656
aimssquad <abbasbitm3655@gmail.com>
2024-12-28 15:44:44 +0530

    updated

--- resources/views/front/visionnaire-detail.blade.php
+++ resources/views/front/visionnaire-detail.blade.php
@@ -3,10 +3,17 @@
 @section('content')
 
 
-<div class="page-title dark-background">
+{{-- <div class="page-title dark-background">
     <div class="container">
         <h1>{{ $data->title }}</h1>
         <h3>{{ $data->sub_title }} </h3>
+    </div> --}}
+
+    <div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/Visionnaire.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
+        <div class="container">
+            <h1>{{ $data->title }}</h1>
+            <h3>{{ $data->sub_title }} </h3>
+        </div>
     </div>
 </div>
     @include('front.sections.visionnaire-detail')
--- resources/views/front/visionnaire.blade.php
+++ resources/views/front/visionnaire.blade.php
@@ -8,9 +8,16 @@
 
 
 @section('content')
-<div class="page-title dark-background">
+{{-- <div class="page-title dark-background">
     <div class="container">
         <h1>Visionnaire </h1>
+        <h3>A Visionary Event in Indian Eyewear</h3>
+    </div>
+</div> --}}
+
+<div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/Visionnaire.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
+    <div class="container">
+        <h1>Visionnaire</h1>
         <h3>A Visionary Event in Indian Eyewear</h3>
     </div>
 </div>
