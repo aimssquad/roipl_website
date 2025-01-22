b7ed614c6ba495ed9b4f5fc6929ae08b6bd24a69
aimssquad <abbasbitm3655@gmail.com>
2025-01-22 15:44:25 +0530

    image update

--- resources/views/front/sections/event.blade.php
+++ resources/views/front/sections/event.blade.php
@@ -30,7 +30,7 @@
             <!-- Insert the banner after the first row of events -->
             @if($index == 0)
                 <div class="events-banner">
-                    <img src="{{ asset('assets/img/lar.jpg') }}" alt="Banner Image" class="img-fluid">
+                    <img src="{{ asset('assets/img/lor_new.jpg') }}" alt="Banner Image" class="img-fluid">
                 </div>
             @endif
         @endforeach
