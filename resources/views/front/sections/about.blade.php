07495cc64b5ca156368b07ee37ae307ec7a33bad
aimssquad <abbasbitm3655@gmail.com>
2025-02-01 12:06:19 +0530

    updated

--- resources/views/front/sections/about.blade.php
+++ resources/views/front/sections/about.blade.php
@@ -5,9 +5,9 @@
         <p style="text-align: justify;font-weight: 500;">Ronak Optik India Private Limited is an eyewear business with over a
             quarter-century of experience in the industry. Established in 1993 with a commitment to quality and customer
             satisfaction, we’ve grown to become one of India’s largest eyewear  distributors, and offer a vast collection
-            of in-house brands such as IDEE, IRUS and IMAGE eyewear, as well as several international brands.
+            of in-house brands such as IDEE, IRUS and IMAGE eyewear, in addition to several international brands.
         <br></p><p style="text-align: justify;font-weight: 500;margin-top:15px;"> Here at Ronak Optik, we believe in serving everyone, from the
-            more discerning buyer to the luxury customer.  At Ronak Optik, you’ll find a
+            more discerning buyer to the luxury customer.  At Ronak Optik, you will find a
             varied range of frames - from quality in-house to international brands. Our extensive collection
              is curated to meet diverse tastes and preferences, guaranteeing that every customer finds their perfect
               fit. Ronak Optik is committed to providing eyewear that combines quality, comfort and style.</p>
--- resources/views/front/sections/hero.blade.php
+++ resources/views/front/sections/hero.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-8">
                 <h2>We Are For Sight</h2>
-                <p>Bringing you the best of world-class eyewear that catches your eye.</p>
+                <p>Bringing You Exceptional Eyewear That Catches Your Eye.</p>
                 {{-- <a href="#about" class="btn-get-started">Get Started</a> --}}
             </div>
         </div>
