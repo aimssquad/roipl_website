2b682a173b1f84a4d6d41f02c82e8e32de9dcf81
aimssquad <abbasbitm3655@gmail.com>
2025-02-26 16:33:10 +0530

    updated

--- resources/views/front/sections/brand-home.blade.php
+++ resources/views/front/sections/brand-home.blade.php
@@ -6,7 +6,7 @@
         <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
             <div class="row align-items-center">
                 <div class="col-lg-12 text-center text-lg-center">
-                    <ul class="portfolio-filters isotope-filters white-text-color" data-aos="fade-up" data-aos-delay="100">
+                    <ul class="portfolio-filters isotope-filters white-text-color" data-aos="fade-up" >
                         <li data-filter="*" class="filter-active">All</li>
                         @foreach($brands->unique('brand_type') as $brand)
                             <li data-filter=".filter-{{ strtolower($brand->brand_type) }}">
@@ -17,7 +17,7 @@
                 </div>
             </div>
 
-            <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="50">
+            <div class="row gy-4 isotope-container" data-aos="fade-up" >
                 @foreach($brands as $brand)
                     <div class="col-lg-3 col-md-6 portfolio-item isotope-item filter-{{ strtolower($brand->brand_type) }}">
                         <div class="portfolio-content h-100">
