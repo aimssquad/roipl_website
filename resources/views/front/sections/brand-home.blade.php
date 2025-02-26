d059f31ff2b4ba0e4ef261e052a98f9c0f49a3c5
aimssquad <abbasbitm3655@gmail.com>
2025-02-26 16:34:30 +0530

    updated

--- resources/views/front/sections/brand-home.blade.php
+++ resources/views/front/sections/brand-home.blade.php
@@ -1,12 +1,12 @@
 <section id="portfolio" class="portfolio section " style="background: url('{{ asset('assets/img/brand2.jpg') }}') no-repeat center center/cover;">
-    <div class="container section-title" data-aos="fade-up">
+    <div class="container section-title" >
         <h1 style="color: white !important;"> Brands</h1>
     </div>
     <div class="container px-6">
         <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
             <div class="row align-items-center">
                 <div class="col-lg-12 text-center text-lg-center">
-                    <ul class="portfolio-filters isotope-filters white-text-color" data-aos="fade-up" >
+                    <ul class="portfolio-filters isotope-filters white-text-color"  >
                         <li data-filter="*" class="filter-active">All</li>
                         @foreach($brands->unique('brand_type') as $brand)
                             <li data-filter=".filter-{{ strtolower($brand->brand_type) }}">
@@ -17,7 +17,7 @@
                 </div>
             </div>
 
-            <div class="row gy-4 isotope-container" data-aos="fade-up" >
+            <div class="row gy-4 isotope-container"  >
                 @foreach($brands as $brand)
                     <div class="col-lg-3 col-md-6 portfolio-item isotope-item filter-{{ strtolower($brand->brand_type) }}">
                         <div class="portfolio-content h-100">
