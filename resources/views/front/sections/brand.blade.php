8726503a8168e48163128e2ef0b1e8a8e5b0b5b2
aimssquad <abbasbitm3655@gmail.com>
2025-02-26 15:54:22 +0530

    updated

--- resources/views/front/sections/brand.blade.php
+++ resources/views/front/sections/brand.blade.php
@@ -1,6 +1,6 @@
 <section id="portfolio" class="portfolio section " style="background: url('{{ asset('assets/img/brand2.jpg') }}') no-repeat center center/cover;">
     <div class="container section-title" data-aos="fade-up">
-        <h1 style="color: white !important;margin-top: 110px;margin-bottom: 70px;">Our Brand</h1>
+        <h1 style="color: white !important;margin-top: 110px;margin-bottom: 70px;"> Brands</h1>
     </div>
     <div class="container px-6">
         <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
