a2c305395591e112c753b0e3af0674d4890cbc69
aimssquad <abbasbitm3655@gmail.com>
2025-02-27 15:26:38 +0530

    updated

--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -1,11 +1,13 @@
 @extends('front.layouts.app')
 
 @section('content')
-<div class="page-title dark-background" style="background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{asset('assets/img/Life-at-ROIPL.jpg')}}'); background-size: cover; background-position: center;height: 70vh;">
+{{-- <div class="page-title dark-background" style="background-image:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{asset('assets/img/Life-at-ROIPL.jpg')}}'); background-size: cover; background-position: center;height: 70vh;">
     <div class="container">
         <h1 style="color: white !important;">Life at ROIPL</h1>
     </div>
-</div>
+</div> --}}
+
+@include('front.partials.page-title', ['title' => 'Life at ROIPL', 'current' => 'Life at ROIPL'])
 
 {{-- <div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
     <div class="container">
