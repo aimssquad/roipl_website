194e51a6b96044d006fe3710ed6d126a864880e2
aimssquad <abbasbitm3655@gmail.com>
2025-03-22 14:54:09 +0530

    updated

--- resources/views/front/careers.blade.php
+++ resources/views/front/careers.blade.php
@@ -1,7 +1,7 @@
 @extends('front.layouts.app')
 
 @section('content')
-    <div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/topBanner/Careers-2.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
+    <div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/Careers-2.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
         <div class="container">
             <h1>Careers</h1>
         </div>
