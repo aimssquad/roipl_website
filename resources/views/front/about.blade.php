58f05dc4b192e5f5e8a47ce806871ad157e08473
aimssquad <abbasbitm3655@gmail.com>
2025-03-22 14:31:14 +0530

    updated

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -1,7 +1,7 @@
 @extends('front.layouts.app')
 
 @section('content')
-<div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/topBanner/Our-Story-top-banner.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
+<div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/topBanner/Our-Story-top-banner-2.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
     <div class="container">
         <h1>Our Story</h1>
     </div>
--- resources/views/front/events.blade.php
+++ resources/views/front/events.blade.php
@@ -1,7 +1,7 @@
 @extends('front.layouts.app')
 
 @section('content')
-<div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/topBanner/Life At ROIPL.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
+<div class="page-title page-title dark-background" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),  url('{{ asset('assets/img/topBanner/Life At ROIPL-2.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 50vh;">
     <div class="container">
         <h1>Life at ROIPL</h1>
     </div>
